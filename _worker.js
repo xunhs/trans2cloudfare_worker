0c1d9a48610517332362bcf61bd83bbc01f3efd3
Ethan <31309468+xunhs@users.noreply.github.com>
2022-09-10 15:42:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,8 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
+    // telegram
+    if (url.pathname.includes('bot1790677966')) {
       url.hostname = 'tg.xunhs.workers.dev'
       let new_request = new Request(url, request);
       return fetch(new_request);
