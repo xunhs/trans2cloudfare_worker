4b43e5282e63e758ddc5ffd5343c692e1718d5cf
Ethan <31309468+xunhs@users.noreply.github.com>
2022-09-10 18:05:14 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,6 +7,12 @@ export default {
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
+    else{
+      // default redirect to blog
+      url.hostname = 'snow.xunhs.workers.dev'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
     return env.ASSETS.fetch(request);
   },
 };
